27218588d2a15fcf104ccf797a974769511de455
Daniel Sarafanov <sarafanovdaniel@gmail.com>
2024-01-09 20:46:38 -0500

    Added Sharp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-      unoptimized: true,
-    },
     // ... any other configurations you might have
   }
   
